fe1c0cdb22f224583bffe3c5d9bb08f76a5fe3a5
tiya-513 <rudrapd2005@gmail.com>
2026-06-20 11:03:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+import matplotlib.pyplot as plt
+
+print("Installed")
